771e0f6b9267b577b6a076a686f037e5de8360e1
Rhoyo <ryanhkho@gmail.com>
2024-03-12 15:41:50 -1000

    Updates

---
+++ component---src-pages-index-tsx-35c0db1fb4987be88aa2.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrhoyo=self.webpackChunkrhoyo||[]).push([[691],{5806:function(e,t,n){n.r(t),n.d(t,{Head:function(){return c},default:function(){return u}});var l=n(7294);var r=()=>l.createElement("div",null,"header");var a=()=>l.createElement("div",null,"Footer");var u=()=>l.createElement("main",null,l.createElement(r,null),"testing my script",l.createElement(a,null));const c=()=>l.createElement("title",null,"Home Page")}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-35c0db1fb4987be88aa2.js.map
